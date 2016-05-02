db591b9a51849a47dbc662390bb759ca19822114
DM Kishi <dm.kishi@gmail.com>
2016-05-02 11:04:07 -0700

    Rename source dir to "src"

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,26 @@
 var gulp   = require('gulp');
+var bump   = require('gulp-bump');
 var jshint = require('gulp-jshint');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+
+var SRC = 'src/*.js';
 
 gulp.task('default', () => {
   gulp.start('jshint', 'build');
 });
 
 gulp.task('jshint', () => {
-  return gulp.src('source/*.js')
+  return gulp.src(SRC)
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('default'));
 });
 
 // Copy source contents first to "dist" then uglify and rename.
 gulp.task('build', () => {
-  return gulp.src('source/*.js')
+  return gulp.src(SRC)
     .pipe(gulp.dest('dist'))
+    .pipe(bump())
     .pipe(uglify())
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('dist'));
