8177eac3334ed01b4d4c453a39d5e0aa8728acc0
DM Kishi <dm.kishi@gmail.com>
2025-02-01 22:50:32 -0800

    refactor: Regularize variable names

--- src/jp.ts
+++ src/jp.ts
@@ -32,18 +32,18 @@ export default function num2Jp(integer: number): string {
  * Japanese numerals are grouped into myriads so we iterate in chunks of four,
  * or quads.
  */
-function iter(numerals: string, cumulativeQuadWords: string = '', index: number = 0): string {
-  const [quad, remainingNumerals] = split(numerals);
-  if (isAllZeroQuad(quad) && remainingNumerals.length === 0) {
+function iter(digits: string, cumulativeQuadWords: string = '', index: number = 0): string {
+  const [quad, remainingDigits] = split(digits);
+  if (isAllZeroQuad(quad) && remainingDigits.length === 0) {
     if (index === 0)
       return '零';
     else
       return cumulativeQuadWords;
   }
   const newQuadWords = getQuadWords(quad);
-  const newCumulativeQuadWords = addQuadWords(newQuadWords, cumulativeQuadWords, index);
+  const newCumulativeQuadWords = accumQuadWords(newQuadWords, cumulativeQuadWords, index);
   const newIndex = index + 1;
-  return iter(remainingNumerals, newCumulativeQuadWords, newIndex);
+  return iter(remainingDigits, newCumulativeQuadWords, newIndex);
 }
 
 function isAllZeroQuad(quad: Quad): boolean {
@@ -66,10 +66,10 @@ function isAllZeroQuad(quad: Quad): boolean {
  * `987654321` | `["98765", "4321"]`
  * `1`         | `["", "0001"]`
  */
-function split(numerals: string): [Quad, string] {
-  const firstQuad = ('0000' + numerals).slice(-4).split('') as Quad;
-  const remainingNumerals = numerals.slice(0, -4);
-  return [firstQuad, remainingNumerals];
+function split(digits: string): [Quad, string] {
+  const firstQuad = ('0000' + digits).slice(-4).split('') as Quad;
+  const remainingDigits = digits.slice(0, -4);
+  return [firstQuad, remainingDigits];
 }
 
 /**
@@ -102,7 +102,7 @@ function getWord(digit: string, place: number): string {
   return CHAR[numDigit] + (SMALL_POWER[place - 1] as string) ;
 }
 
-function addQuadWords(newQuadWords: string, cumulativeQuadWords: string, index: number): string {
+function accumQuadWords(newQuadWords: string, cumulativeQuadWords: string, index: number): string {
   const scaleWord = LARGE_POWER[index];
   if (scaleWord !== '' && newQuadWords === '千') {
     return '一' + newQuadWords + scaleWord + cumulativeQuadWords;
