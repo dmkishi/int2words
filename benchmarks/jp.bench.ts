8b65d18e9b5d07e8c25b7cdc8609d7e5c59340d5
DM Kishi <dm.kishi@gmail.com>
2025-02-02 19:33:42 -0800

    build: Add benchmark test

---
+++ benchmarks/jp.bench.ts
@@ -0,0 +1,18 @@
+import { bench, describe } from 'vitest';
+import num2Jp from '../src/jp.js';
+
+describe('transform smallest supported integer', () => {
+  bench('zero', () => {
+    num2Jp(0);
+  });
+});
+
+describe('transform largest supported integer', () => {
+  bench('exponent', () => {
+    num2Jp(2**53);
+  });
+
+  bench('literal', () => {
+    num2Jp(9007199254740992);
+  });
+});
