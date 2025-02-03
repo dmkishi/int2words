794ed323ddac7c1a9d313ab234c48dd61485daf8
DM Kishi <dm.kishi@gmail.com>
2025-02-02 18:41:42 -0800

    feat: Add Error cause

--- src/util.ts
+++ src/util.ts
@@ -6,7 +6,7 @@ class ArgumentError extends Error {
 
 export function validateInteger(integer: number): void {
   if (!Number.isInteger(integer))
-    throw new ArgumentError('Expected an integer.');
+    throw new ArgumentError('Expected an integer.', { cause: integer });
   if (integer < 0)
-    throw new ArgumentError('Expected a positive integer.');
+    throw new ArgumentError('Expected a positive integer.', { cause: integer });
 }
