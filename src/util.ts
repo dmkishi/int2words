c3b8fda25995e09245a256843b995fa9a508cb52
DM Kishi <dm.kishi@gmail.com>
2025-02-02 18:41:10 -0800

    refactor: Switch to custom error class

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,12 @@
+class ArgumentError extends Error {
+  constructor(message: string, options: object = {}) {
+    super(`Invalid argument: ${message}`, options);
+  }
+}
+
 export function validateInteger(integer: number): void {
   if (!Number.isInteger(integer))
-    throw new Error('Invalid argument: expected an integer.');
+    throw new ArgumentError('Expected an integer.');
   if (integer < 0)
-    throw new Error('Invalid argument: expected a positive integer.');
+    throw new ArgumentError('Expected a positive integer.');
 }
