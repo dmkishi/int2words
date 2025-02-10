1b8a8970460c89bafa09351170866645f7e7644f
DM Kishi <dm.kishi@gmail.com>
2025-02-10 12:00:32 -0800

    build: Enable ESLint with type information

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,49 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
+  {
+    /**
+     * Global ignores. If `ignores` is used without any other keys in the
+     * configuration object, then the patterns act as global ignores and it gets
+     * applied to every configuration object.
+     * @see https://eslint.org/docs/latest/use/configure/configuration-files#configuration-objects
+     */
+    ignores: [
+      'benchmarks/',
+      'dist/',
+      'tests/',
+      'eslint.config.mjs'
+    ],
+  },
   eslint.configs.recommended,
-  tseslint.configs.strict,
-  tseslint.configs.stylistic,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    /**
+     * Enable linting with type information. E.g.
+     * - tseslint.configs.recommended → tseslint.configs.recommendedTypeChecked
+     * - tseslint.configs.stylistic → tseslint.configs.stylisticTypeChecked
+     * @see https://typescript-eslint.io/getting-started/typed-linting
+     */
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      "@typescript-eslint/restrict-template-expressions": [
+        "error",
+        {
+          "allowNumber": true,
+          "allowBoolean": false,
+          "allowAny": false,
+          "allowNullish": false,
+          "allowRegExp": false
+        }
+      ],
+    },
+  },
 );
