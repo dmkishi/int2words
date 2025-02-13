49886abb92bf01642153bec2765e20ea58de2262
DM Kishi <dm.kishi@gmail.com>
2025-02-12 20:33:40 -0800

    style: Add missing hanging comma

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,7 @@ export default tseslint.config(
       'benchmarks/',
       'dist/',
       'tests/',
-      'eslint.config.mjs'
+      'eslint.config.mjs',
     ],
   },
   eslint.configs.recommended,
